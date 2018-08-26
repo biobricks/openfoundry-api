b1d791f5b6a9bb7fbe498de24506b55f0c0ec2f5
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-08-25 18:18:46 -0700

    add config class

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
-
+from config import Config
 import sqlite3
 
 # create a flask application and assign to app variable
 app = Flask(__name__)
+
+# creates app configuration based on our config class
+app.config.from_object(Config)
 
 # set debugging to True to show errors on the screen, rather than Bad Gateway message
 app.config["DEBUG"] = True
