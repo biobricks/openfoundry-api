f70d4f6015bbd4551a1d3c606d674c3772b0302c
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-08-26 21:44:31 -0700

    add username validation to edit user

--- app/routes.py
+++ app/routes.py
@@ -111,7 +111,7 @@ def user(username):
 @login_required
 def edit_profile():
   title = "Edit Profile"
-  form = EditProfileForm()
+  form = EditProfileForm(current_user.username)
   if form.validate_on_submit():
     current_user.username = form.username.data
     current_user.bio = form.bio.data
